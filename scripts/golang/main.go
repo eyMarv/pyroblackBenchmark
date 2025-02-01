1cb927a239391729bee378c917269e4f3dcc284c
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2025-02-02 00:26:44 +0530

    rename directories

--- scripts/golang/main.go
+++ scripts/golang/main.go
@@ -124,8 +124,7 @@ func main() {
 	benchmark.FileSize = fileSize
 
 	jsonBenchmark, _ := json.MarshalIndent(benchmark, "", "  ")
-	os.Mkdir("outputs", 0755)
-	os.WriteFile("outputs/gogram.json", jsonBenchmark, 0644)
+	os.WriteFile("../../outputs/gogram.json", jsonBenchmark, 0644)
 }
 
 func HumanizeBytes(size int64) string {
