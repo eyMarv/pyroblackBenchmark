d15414c1e81eac0d4950bbedcff266831bda1ea5
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2025-02-02 00:34:41 +0530

    (merge): rename directories

--- run.sh
+++ run.sh
@@ -3,17 +3,17 @@
 pip install --upgrade pip > /dev/null 2>&1
 mkdir -p outputs
 
-# echo "Beginning Telethon test"
-# pip install --force-reinstall https://github.com/lonamiwebs/telethon/archive/a2926b5.zip cryptg==0.5.0.post0 > /dev/null 2>&1
-# T=$(python scripts/python/t1.py)
-# pip uninstall -y telethon cryptg > /dev/null 2>&1
-# echo "${T}" >> outputs/telethon.json
+echo "Beginning Telethon test"
+pip install --force-reinstall https://github.com/lonamiwebs/telethon/archive/a2926b5.zip cryptg==0.5.0.post0 > /dev/null 2>&1
+T=$(python scripts/python/t1.py)
+pip uninstall -y telethon cryptg > /dev/null 2>&1
+echo "${T}" >> outputs/telethon.json
 
-# echo "Beginning Pyrogram test"
-# pip install --force-reinstall https://github.com/TelegramPlayGround/Pyrogram/archive/26c2d73.zip https://github.com/TelegramPlayGround/pyrogram-tgcrypto/archive/10142c3.zip > /dev/null 2>&1
-# P=$(python scripts/python/p2.py)
-# pip uninstall -y pyrotgfork pytgcrypto > /dev/null 2>&1
-# echo "${P}" >> outputs/pyrotgfork.json
+echo "Beginning Pyrogram test"
+pip install --force-reinstall https://github.com/TelegramPlayGround/Pyrogram/archive/26c2d73.zip https://github.com/TelegramPlayGround/pyrogram-tgcrypto/archive/10142c3.zip > /dev/null 2>&1
+P=$(python scripts/python/p2.py)
+pip uninstall -y pyrotgfork pytgcrypto > /dev/null 2>&1
+echo "${P}" >> outputs/pyrotgfork.json
 
 echo "Beginning GoGram test"
 cd scripts/golang/
