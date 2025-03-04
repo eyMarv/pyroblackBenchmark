d1474df6390748a45bea413b8a782b78fc9c7220
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2025-03-04 21:03:34 +0530

    (fix): formatSpeed

--- index.js
+++ index.js
@@ -41,9 +41,42 @@ const libs = [
     },
 ];
 
-function formatSpeed(bytes, seconds) {
-    const mbps = (bytes / 1024 / 1024 / seconds).toFixed(2);
-    return `${mbps} MB/s`;
+/**
+ * Format bytes as human-readable text.
+ *
+ * @param bytes Number of bytes.
+ * @param si True to use metric (SI) units, aka powers of 1000. False to use
+ *           binary (IEC), aka powers of 1024.
+ * @param dp Number of decimal places to display.
+ *
+ * @return Formatted string.
+ *
+ * https://stackoverflow.com/a/14919494/4723940
+ */
+function formatSpeed(bytes, seconds, si = false, dp = 2) {
+    if (!bytes) {
+        return "";
+    }
+    const thresh = si ? 1000 : 1024;
+
+    if (Math.abs(bytes) < thresh) {
+        return (bytes / seconds).toFixed(dp) + " B/s";
+    }
+
+    const units = si
+        ? ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+        : ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    let u = -1;
+    const r = 10 ** dp;
+
+    do {
+        bytes /= thresh;
+        ++u;
+    } while (
+        Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1
+    );
+
+    return "" + (bytes / seconds).toFixed(dp) + " " + units[u] + "/s";
 }
 
 function calculateScore(data) {
