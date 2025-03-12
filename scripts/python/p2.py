afeaab2c908130b66ef33633be40faf3f72d38e3
Marvin <eymarv07@gmail.com>
2025-03-12 09:47:30 +0100

    Update p2.py

--- scripts/python/p2.py
+++ scripts/python/p2.py
@@ -1,4 +1,7 @@
 import asyncio
+import uvloop
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
 import logging
 import os
 from datetime import datetime
@@ -29,10 +32,10 @@ TG_MESSAGE_LINK = os.environ.get("TG_MESSAGE_LINK", "")
 
 async def main():
     d = {}
+    # bot account
+    logging.warning("--- STARTING BOT BENCHMARK ---")
     async with Client(
-        name="my_account",
-        session_string=TG_SESSION,
-        in_memory=True,
+        name="my_bot",
         api_id=TG_API_ID,
         api_hash=TG_API_HASH,
         sleep_threshold=TG_FLOOD_SLEEP_THRESHOLD,
@@ -61,7 +64,7 @@ async def main():
         await app.send_document(
             chat_id=message.chat.id,
             document=filename,
-            caption="Pyrogram",
+            caption="Pyroblack bot",
             reply_to_message_id=message.id
         )
         t5 = datetime.now()
@@ -70,6 +73,37 @@ async def main():
             "end_time": t5.timestamp(),
             "time_taken": (t5 - t4).seconds
         }
+        os.remove(filename)
+    
+
+    # user account
+    logging.warning("--- STARTING USER BENCHMARK ---")
+    async with Client(
+        name="my_account",
+        session_string=TG_SESSION,
+        api_id=TG_API_ID,
+        api_hash=TG_API_HASH,
+        sleep_threshold=TG_FLOOD_SLEEP_THRESHOLD,
+        no_updates=True,
+    ) as app:
+        app.upload_boost = True
+        chat_id, s_message_id = TG_MESSAGE_LINK.split("/")
+
+        t1 = datetime.now()
+        message = await app.get_messages(chat_id=chat_id, message_ids=int(s_message_id))
+        t2 = datetime.now()
+        filename = await message.download()
+        t3 = datetime.now()
+        d["download"]["time_taken"] = (int(d["download"]["time_taken"]) + int((t3 - t2).seconds))) / 2
+        t4 = datetime.now()
+        await app.send_document(
+            chat_id=message.chat.id,
+            document=filename,
+            caption="Pyroblack user",
+            reply_to_message_id=message.id
+        )
+        t5 = datetime.now()
+        d["upload"]["time_taken"] = (int(d["download"]["time_taken"]) + int((t5 - t4).seconds))) / 2
         os.remove(filename)
     print(dumps(d, indent=2))
 
